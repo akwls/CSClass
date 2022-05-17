6e9af0616bb685e5de6c0691e369fd744231d849
akwls <rhrnak624@naver.com>
2022-05-17 11:55:47 +0900

    #1 사용자 정의 Car 클래스 제작 추가로 ///xml 메서드 주석도 배움

---
+++ CSClass/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass
+{
+    internal class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        //<summary>
+        //자동차가 들어온 시간 설정
+        //</summary>
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        //<summary>
+        //자동차가 나간 시간 설정
+        //</summary>
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+    }
+}
