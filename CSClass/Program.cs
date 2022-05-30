86fc487a12aeebc29d8f8481779e65db6493720c
akwls <rhrnak624@naver.com>
2022-05-30 14:48:36 +0900

    #2 Math 클래스 메서드 사용법

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -30,6 +30,14 @@ namespace CSClass
                 Console.WriteLine("Count : " + list.Count + "\t" + item);
             }
 
+            
+            Console.WriteLine(Math.Abs(-219));
+            Console.WriteLine(Math.Ceiling(2.19));
+            Console.WriteLine(Math.Floor(21.9));
+            Console.WriteLine(Math.Round(21.5));
+            Console.WriteLine(Math.Max(2, 19));
+            Console.WriteLine(Math.Min(21, 9));
+            Console.WriteLine(Math.PI);
         }
     }
 }
