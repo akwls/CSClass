166003c3c883591bf24e063e03670ce73218f998
akwls <rhrnak624@naver.com>
2022-05-30 14:44:44 +0900

    #2 List 클래스 사용법

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -21,6 +21,15 @@ namespace CSClass
             Console.WriteLine(random.Next(10, 100));
             Console.WriteLine(random.Next(10, 100));
 
+            List<int> list = new List<int>() { 12, 24, 36, 48, 60};
+            list.Remove(48);
+            list.RemoveAt(0);
+
+            foreach(int item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\t" + item);
+            }
+
         }
     }
 }
