bada8d49b453b2f9d7e39b31fe3bdc620111cd3d
akwls <rhrnak624@naver.com>
2022-05-31 11:39:30 +0900

    #6 클래스 응용

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -51,6 +51,70 @@ namespace CSClass
             Console.WriteLine(product1);
             Console.WriteLine(product2);
             Console.WriteLine(product3);
+
+            List<Student> students = new List<Student>()
+            {
+                new Student() { name = "함기훈", grade = 100 },
+                new Student() { name = "가연우", grade = 100 },
+                new Student() { name = "김유나", grade = 100 },
+                new Student() { name = "김유진", grade = 99 },
+                new Student() { name = "김하진", grade = 80 },
+                new Student() { name = "박수연", grade = 101 },
+                new Student() { name = "박혜원", grade = 77 },
+                new Student() { name = "심이진", grade = 100 },
+                new Student() { name = "유예영", grade = 33 },
+                new Student() { name = "윤주희", grade = 22 },
+                new Student() { name = "윤채원", grade = 88 },
+            };
+            // 마지막 데이터에 , 가 있어도 오류 발생하지 않음.
+
+
+            //           students.Add(new Student() { name = "함기훈", grade = 100 });
+            //           students.Add(new Student() { name = "가연우", grade = 100 });
+            //           students.Add(new Student() { name = "김유나", grade = 100 });
+            //           students.Add(new Student() { name = "김유진", grade = 99 });
+            //           students.Add(new Student() { name = "김하진", grade = 80 });
+            //           students.Add(new Student() { name = "박수연", grade = 101 });
+            //           students.Add(new Student() { name = "박혜원", grade = 77 });
+            //           students.Add(new Student() { name = "심이진", grade = 100 });
+            //           students.Add(new Student() { name = "우예영", grade = 33 });
+            //           students.Add(new Student() { name = "윤주희", grade = 22 });
+            //           students.Add(new Student() { name = "윤채원", grade = 88 });
+
+            // 점수가 60점 이상이 넘는 사람은 제거하기
+            /*
+            foreach(var student in students)
+            {
+                if(student.grade > 60)
+                {
+                    students.Remove(student);
+                }
+            } 
+            */
+/*
+            for(int i = 0; i<students.Count; i++)
+            {
+                if (students[i].grade > 60)
+                {
+                    students.RemoveAt(i);
+                    i--;
+                }
+            }
+*/
+
+            for (int i = students.Count-1; i >= 0; i--)
+            {
+                if (students[i].grade > 60)
+                {
+                    students.RemoveAt(i);
+                }
+            }
+
+            foreach (var item in students)
+            {
+                // Console.WriteLine(item.name + " : " + item.grade);
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ CSClass/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace CSClass
+{
+    class Student
+    {
+        // 학생 이름
+        public string name;
+
+        // 학생 성적
+        public int grade;
+
+        // 학생 객체 출력 메서드
+        public override string ToString()
+        {
+            return this.name + " : " + this.grade;
+        }
+
+
+    }
+}
