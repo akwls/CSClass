66a032fa677f2fc62a26316f4a043960a8fa9877
akwls <rhrnak624@naver.com>
2022-06-14 09:10:45 +0900

    22-9. 메서드 응용 - 재귀, 메모라이제이션 #18

---
+++ CSClass/Fibbonacci.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CSClass
+{
+    public class Fibbonacci
+    {
+        private static Dictionary<int, long> memo = new Dictionary<int, long>();
+
+        public Fibbonacci()
+        {
+
+        }
+
+        public static long Get(int n)
+        {
+            if (n < 0) return 0;
+            if (n == 1) return 1;
+
+            if(memo.ContainsKey(n))
+            {
+                return memo[n];
+            }
+            else
+            {
+                long value = Get(n - 2) + Get(n - 1);
+                memo[n] = value;
+                return value;
+            }
+           
+        }
+    }
+}
